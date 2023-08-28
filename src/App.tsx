10647bcf83f1fa4cd61187ec16720c7d9d2e1ab8
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-08-29 04:13:03 +0530

    Creating of New Note

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import { useEditor, EditorContent } from "@tiptap/react";
+import { Content, useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import styles from "./App.module.css";
+import { useState } from "react";
+import { v4 as uuid } from "uuid"
+
+type Note = {
+  id: string;
+  title: string;
+  content: Content;
+  updatedAt: Date;
+};
 
 function App() {
+  const [notes, setNotes] = useState<Record<string, Note>>({});
   const editor = useEditor({
     extensions: [StarterKit],
     content: "<p>Hello World</p>",
@@ -19,10 +29,15 @@ function App() {
   const toggleItalic = () => {
     editor?.chain().focus().toggleItalic().run();
   };
+  const handleCreateNewNote = () => {};
 
   return (
     <div className={styles.pageContainer}>
-      <div className={styles.sidebar}>Sidebar</div>
+      <div className={styles.sidebar}>
+        <button className={styles.sidebarButton} onClick={handleCreateNewNote}>
+          New Note
+        </button>
+      </div>
       <div className={styles.editorContainer}>
         <div className={styles.toolbar}>
           <button
